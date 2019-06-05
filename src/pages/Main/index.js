8f9c8af51f81389fc4cf2834446a08ac03cd08ea
yzy <yanzengyong@cetcbigdata.com>
2019-06-05 18:02:18 +0800

    'init:init'

---
+++ src/pages/Main/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { directive } from '@babel/types';
+
+// const MainPage = () => {
+// 	return (
+// 		<div>
+// 			<div>dasdas</div>
+
+// 		</div>
+// 	)
+// }
+// class Main extends React.Component {
+//   render () {
+//     return (
+//       <div></div>
+//     )
+//   }
+// }
+
+// export default Main
+function a () {
+  return (
+    <div>
+      <p>hello asdasdsd</p>
+    </div>
+  )
+}
+
+export default a
---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+/**
+ * 定义应用路
+ */
+import React from 'react'
+import { Route, Redirect, Switch, HashRouter as Router } from 'react-router-dom'
+import {
+	Main
+} from './routes'
+
+const { RouterHandler } = Router
+
+const routes = () => (
+	<Router>
+		<Switch>
+			<Route exact path="/" render={() => (
+				<Redirect to="/main"/>
+			)}/>
+			<Route path='/main' component={Main}/>
+			{/* <Route path='/standardDetail' component={StandardDetail}/> */}
+		</Switch>
+	</Router>
+)
+
+export default routes
