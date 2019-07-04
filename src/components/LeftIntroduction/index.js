1916c43d7aafdb52490a03aab2f3143b676445db
zhangyao <zhangyao@cetcbigdata.com>
2019-07-04 16:26:51 +0800

    Feat：大屏左边设计完成

--- src/components/LeftIntroduction/index.js
+++ src/components/LeftIntroduction/index.js
@@ -9,7 +9,36 @@ export default class LeftIntroduction extends React.Component {
 	}
 	render () {
 		return(
-			<div></div>
+			<div className="LeftIntro">
+				<div className="firstIntro">
+					<img src="assets/images/fl.png" className="firstIntroImg"></img>
+					<div className="firstIntroText">
+						<h1>分类分级</h1>
+						<div>介绍比比比比比比比比比</div>
+					</div>
+				</div>
+				<div className="firstIntro">
+					<div className="secondIntroText">
+						<h1 className="modelText">数据模型</h1>
+						<div>介绍比比比比比比比比比</div>
+					</div>
+					<img src="assets/images/model.png" className="secondIntroImg"></img>
+				</div>
+				<div className="firstIntro">
+					<img src="assets/images/lock.png" className="thirdIntroImg"></img>
+					<div >
+						<h1 className="lockText">安全加密</h1>
+						<div>介绍比比比比比比比比比</div>
+					</div>
+				</div>
+				<div className="firstIntro">
+					<div className="secondIntroText">
+						<h1 className="modelText">共享开放</h1>
+						<div>介绍比比比比比比比比比</div>
+					</div>
+					<img src="assets/images/share.png" className="secondIntroImg"></img>
+				</div>
+			</div>
 		)
 	}
 }
