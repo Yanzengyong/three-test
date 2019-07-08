5c3be9540524af64c948549a364b3f0998de9412
zhangyao <zhangyao@cetcbigdata.com>
2019-07-08 09:33:31 +0800

    Feat：分类分级、数据模型等名词解释

--- src/components/LeftIntroduction/index.js
+++ src/components/LeftIntroduction/index.js
@@ -14,13 +14,13 @@ export default class LeftIntroduction extends React.Component {
 					<img src="assets/images/fl.png" className="firstIntroImg"></img>
 					<div className="firstIntroText">
 						<h1>分类分级</h1>
-						<div>介绍比比比比比比比比比</div>
+						<div className="flfj">分类分级从隐私安全与保护成本的角度出发，对数据进行分类和等级划分</div>
 					</div>
 				</div>
 				<div className="firstIntro">
 					<div className="secondIntroText">
 						<h1 className="modelText">数据模型</h1>
-						<div>介绍比比比比比比比比比</div>
+						<div className="flfj">数据模型从抽象层次上描述了系统的静态特征、动态行为和约束条件</div>
 					</div>
 					<img src="assets/images/model.png" className="secondIntroImg"></img>
 				</div>
@@ -28,13 +28,13 @@ export default class LeftIntroduction extends React.Component {
 					<img src="assets/images/lock.png" className="thirdIntroImg"></img>
 					<div >
 						<h1 className="lockText">安全加密</h1>
-						<div>介绍比比比比比比比比比</div>
+						<div className="flfj">数据安全加密利用密码技术对信息进行加密，实现信息隐蔽，从而起到保护信息的安全的作用。</div>
 					</div>
 				</div>
 				<div className="firstIntro">
 					<div className="secondIntroText">
 						<h1 className="modelText">共享开放</h1>
-						<div>介绍比比比比比比比比比</div>
+						<div className="flfj">打破数据孤岛，法律讨论先行，技术堡垒破除，人财物保障持续跟进</div>
 					</div>
 					<img src="assets/images/share.png" className="secondIntroImg"></img>
 				</div>
