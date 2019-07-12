b7c0b08fe1e614c60bd70152a5f411752f797c2d
shenling <shenling@cetcbigdata.com>
2019-07-12 17:46:37 +0800

    Feat: 数据治理第二步-数据资产

--- src/components/DataGovernance/index.js
+++ src/components/DataGovernance/index.js
@@ -2,17 +2,44 @@ import React from 'react'
 import ReactEcharts from 'echarts-for-react'
 import './scss/index.scss'
 import { Carousel } from 'antd'
+
+const assestHistory = [
+	['2017-02-01', 34],
+	['2017-03-01', 184],
+	['2017-04-01', 173],
+	['2017-05-01', 106],
+	['2017-06-01', 84],
+	['2017-07-01', 196],
+	['2017-08-01', 223],
+	['2017-09-01', 176],
+	['2017-10-01', 189],
+	['2017-11-01', 286],
+	['2017-12-01', 254],
+	['2018-01-01', 297],
+	['2018-02-01', 320],
+	['2018-03-01', 307],
+	['2018-04-01', 358],
+	['2018-05-01', 315],
+	['2018-06-01', 298],
+	['2018-07-01', 394],
+	['2018-08-01', 378],
+	['2018-09-01', 402],
+	['2018-10-01', 478],
+	['2018-11-01', 425],
+	['2018-12-01', 418],
+	['2019-01-01', 432],
+	['2019-02-01', 401],
+	['2019-03-01', 401],
+	['2019-04-01', 428],
+	['2019-05-01', 467],
+	['2019-06-01', 457]
+]
 export default class DataGovernance extends React.Component {
 	constructor (props) {
 		super(props)
 		this.state={
-			step: 1,
-			stepName: '数据元'
+			step: 3
 		}
-	}
-
-	componentDidMount () {
-
 	}
 	
 	render () {
@@ -97,7 +124,6 @@ export default class DataGovernance extends React.Component {
 		}
 
 		const option_metaDataSourceMethod = {
-
 			title: {
 				text: '数据来源方式分布',
 				left: 'center',
@@ -160,6 +186,200 @@ export default class DataGovernance extends React.Component {
 			]
 		}
 
+		const option_standardDataModel = {
+			title: {
+				text: '标准数据模型占比',
+				left: 'center',
+				bottom: 60,
+				textStyle: {
+					color: '#4AFFFE'
+				}
+			},
+
+			tooltip : {
+				trigger: 'item',
+				formatter: '{b} : {d}%'
+			},
+			series: [
+				{
+					type:'pie',
+					radius : '50%',
+					center: ['50%', '30%'],
+					data:[
+						{ value:32, name:'标准数据模型' },
+						{ value:74, name:'非标准数据模型' },
+					],
+					itemStyle: {
+						normal: {
+							color: '#70AFCE',
+							shadowBlur: 500,
+							shadowColor: 'rgba(0, 0, 1, 0.9)'
+						}
+					}
+				}
+			]
+		}
+
+		const option_standardDataModelMethod = {
+			title: {
+				text: '数据模型生成方式',
+				textStyle: {
+					color: '#4AFFFE'
+				}
+			},
+			tooltip: {
+				trigger: 'axis',
+				axisPointer: {
+					type: 'shadow'
+				}
+			},
+			xAxis: {
+				name: '数据模型（个）',
+				type: 'value',
+				boundaryGap: [0, 0.01],
+				axisLabel:{ color:'#fff' },
+				axisLine: {
+					lineStyle: {
+						color: '#fff'
+					}
+				},
+			},
+			yAxis: {
+				name: '数据模型生成方式',
+				type: 'category',
+				axisLabel:{ color:'#fff' },
+				axisLine: {
+					lineStyle: {
+						color: '#fff'
+					}
+				},
+				data: ['自动抽取', '智能生成', '手动添加'],
+			},
+			series: [
+				{
+					name: '数据模型（个）',
+					type: 'bar',
+					data: [32, 18, 56],
+					itemStyle: {
+						color: '#A5DEF1'
+					}
+				}
+			]
+		}
+
+		const option_assetTheme = {
+			title: {
+				text: '数据资产主题库分布',
+				left: 'center',
+				bottom: 40,
+				textStyle: {
+					color: '#4AFFFE'
+				}
+			},
+
+			tooltip : {
+				trigger: 'item',
+				formatter: '{a} <br/>{b} : {d}%'
+			},
+
+			visualMap: {
+				show: false,
+				min: 80,
+				max: 600,
+				inRange: {
+					colorLightness: [0, 1]
+				}
+			},
+			series : [
+				{
+					name: '主题库',
+					type:'pie',
+					radius : '55%',
+					center: ['50%', '40%'],
+					data:[
+						{ value:200, name:'综合政务' },
+						{ value:264, name:'经济管理' },
+						{ value:342, name:'国土资源' },
+						{ value:412, name:'信息产业' },
+						{ value:387, name:'环境保护' },
+						{ value:297, name:'农业水利' },
+						{ value:186, name:'商业贸易' },
+						{ value:560, name:'其他' },
+					],
+					roseType: 'radius',
+					label: {
+						normal: {
+							textStyle: {
+								color: '#ffffff'
+							}
+						}
+					},
+					labelLine: {
+						normal: {
+							lineStyle: {
+								color: '#ffffff'
+							},
+							smooth: 0.2,
+							length: 10,
+							length2: 20
+						}
+					},
+					itemStyle: {
+						normal: {
+							color: '#3B7B9A',
+							shadowBlur: 200,
+							shadowColor: 'rgba(0, 0, 0, 0.5)'
+						}
+					}
+				}
+			]
+		}
+
+		const option_assestChange = {
+			title: {
+				text: '数据资产规模历史变化',
+				left: 'center',
+				top: 10,
+				textStyle: {
+					color: '#4AFFFE'
+				}
+			},
+
+			tooltip : {
+				trigger: 'axis',
+				axisPointer: {
+					type: 'cross',
+				}
+			},
+			visualMap: [{
+				show: false,
+				type: 'continuous',
+				seriesIndex: 0,
+				min: 0,
+				max: 400
+			}],
+			xAxis: {
+				type: 'category',
+				axisLabel:{ color:'#fff' }
+			},
+			yAxis: {
+				type: 'value',
+				splitLine: { show: false },
+				axisLabel:{ color:'#fff' },
+			},
+			series: [{
+				type: 'line',
+				lineStyle: {
+					color: '#70AFCE'
+				},
+				itemStyle: {
+					color: '#4AFFFE',
+					opacity: 0.6
+				},
+				data: assestHistory
+			}]
+		}
+
 		const stepOne = (
 			<div className='stepContainer'>
 				<div className='chartsContainer'>
@@ -180,67 +400,166 @@ export default class DataGovernance extends React.Component {
 					</div>
 				</div>
 				<div className='chartsContainer'>
-					<div className='dataQuality'>
-
-					</div>
 					<div className='dataQualityQuestion'>
-						<div className='title'>数据质量问题</div>
+						<div className='title'>数据质量主要问题</div>
 						<Carousel dotPosition="left" autoplay autoplayInterval={500} dots={false} style={{ height: '100%' }}>
-  					<div className="BOX">
-						1. 收到XX委办局XX相关数据XX条。
-  					</div>
-  					<div className="BOX">
-						2. 1-3月平台数据源新增XX条。
-  					</div>
-  					<div className="BOX">
-					  3. 1-3月平台数据资源新增XX条。
-  					</div>
-  					<div className="BOX">
-					  4. 1-3平台数据资源总量同比前期涨幅为3.5%
-  					</div>
-  					<div className="BOX">
-            5. 2018年平台数据共支撑38个项目，其中包含全国一体化项目等...
-  					</div>
-  					<div className="BOX">
-					  6. 2018年平台共建有效数据模型189个。其中用于平台支撑的有80个，科研支撑有20个。
-  					</div>
-  					<div className="BOX">
-						7. 2018年平台使用人数达1.5w。
-  					</div>
-  					<div className="BOX">
-					  8. 2019上半年平台新增数据量高达3.5G。
-  					</div>
-  					<div className="BOX">
-					  9. 2019上半年平台安全质量通过国家标准检测。
-  					</div>
-  			</Carousel>
+							<div className="BOX">
+							1. 非标准数据类型较多
+							</div>
+							<div className="BOX">
+							2. 数据不全面
+							</div>
+							<div className="BOX">
+							3. 数据不准确，存在错误数据
+							</div>
+							<div className="BOX">
+							4. 网报人员培训不足，网报数据过程出错，需多次更正
+							</div>
+						</Carousel>
 					</div>
-					<div className='dataSecurity'>
-
+					<div className='dataIndex'>
+						<div className='name'>数据安全指数</div>
+						<div className='score'>
+							<span className='num'>54</span>
+							<span className='unit'>分</span>
+						</div>
+					</div>
+					<div className='dataIndex'>
+						<div className='name'>数据质量指数</div>
+						<div className='score'>
+							<span className='num'>58</span>
+							<span className='unit'>分</span>
+						</div>
 					</div>
 				</div>
 			</div>
 		)
+
 		const stepTwo = (
-			<div className='stepContainer'>two</div>
+			<div className='stepContainer'>
+				<div className='chartsContainer'>
+					<div className='metaDataTotalCount'>
+						<div className='num'>106</div>
+						<div className='name'>数据模型总数</div>
+					</div>
+					<div className='chart'>
+						<ReactEcharts option={option_standardDataModel}/>
+					</div>
+				</div>
+				<div className='chartsContainer'>
+					<div className='chartModel'>
+						<ReactEcharts option={option_standardDataModelMethod}/>
+					</div>
+				</div>
+				<div className='chartsContainer'>
+					<div className='dataQualityQuestion'>
+						<div className='title'>数据质量主要问题</div>
+						<Carousel dotPosition="left" autoplay autoplayInterval={500} dots={false} style={{ height: '100%' }}>
+							<div className="BOX">
+							1. 非标准数据类型较多
+							</div>
+							<div className="BOX">
+							2. 数据不全面
+							</div>
+							<div className="BOX">
+							3. 数据不准确，存在错误数据
+							</div>
+							<div className="BOX">
+							4. 网报人员培训不足，网报数据过程出错，需多次更正
+							</div>
+						</Carousel>
+					</div>
+					<div className='dataIndex'>
+						<div className='name'>数据安全指数</div>
+						<div className='score'>
+							<span className='num'>72</span>
+							<span className='unit'>分</span>
+						</div>
+					</div>
+					<div className='dataIndex'>
+						<div className='name'>数据质量指数</div>
+						<div className='score'>
+							<span className='num'>69</span>
+							<span className='unit'>分</span>
+						</div>
+					</div>
+				</div>
+			</div>
 		)
 		const stepThree = (
-			<div className='stepContainer'>three</div>
-		)
-  	return (
-  		<div className="dataGovernance">
-  			<div className="maintitle">
-					数据治理概况 - {this.state.stepName}
-					{/* <button onClick={()=>{ this.setState({ step: 1, stepName: '数据元' }) }}>数据元</button>
-				<button onClick={()=>{ this.setState({ step: 2, stepName: '数据模型' }) }}>数据模型</button>
-				<button onClick={()=>{ this.setState({ step: 3, stepName: '数据资产' }) }}>数据资产</button> */}
+			<div className='stepContainer'>
+				<div className='chartsContainer'>
+					<div className='metaDataTotalCount'>
+						<div className='num'>
+							<span>457</span>
+							<span className='unit'>GB</span>
+						</div>
+						<div className='name'>数据资产总数</div>
+					</div>
+					<div className='metaDataTotalCount'>
+						<div className='num'>
+							<span>22</span>
+							<span className='unit'>元/月</span>
+						</div>
+						<div className='name'>数据资产管理成本</div>
+					</div>
 				</div>
-  			<div className='divider'/>
+				<div className='chartsContainer'>
+					<div className='chart'>
+						<ReactEcharts option={option_assetTheme}/>
+					</div>
+					<div className='chart'>
+						<ReactEcharts option={option_assestChange}/>
+					</div>
+				</div>
+				<div className='chartsContainer'>
+					<div className='dataQualityQuestion'>
+						<div className='title'>数据质量主要问题</div>
+						<Carousel dotPosition="left" autoplay autoplayInterval={500} dots={false} style={{ height: '100%' }}>
+							<div className="BOX">
+							1. 非标准数据类型较多
+							</div>
+							<div className="BOX">
+							2. 数据不全面
+							</div>
+							<div className="BOX">
+							3. 数据不准确，存在错误数据
+							</div>
+							<div className="BOX">
+							4. 网报人员培训不足，网报数据过程出错，需多次更正
+							</div>
+						</Carousel>
+					</div>
+					<div className='dataIndex'>
+						<div className='name'>数据安全指数</div>
+						<div className='score'>
+							<span className='num'>84</span>
+							<span className='unit'>分</span>
+						</div>
+					</div>
+					<div className='dataIndex'>
+						<div className='name'>数据质量指数</div>
+						<div className='score'>
+							<span className='num'>86</span>
+							<span className='unit'>分</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+		return (
+			<div className="dataGovernance">
+				<div className="maintitle">
+					{this.state.step === 1 ? (<span>数据治理概况 - 数据元</span>) : null} 
+					{this.state.step === 2 ? (<span>数据治理概况 - 数据模型</span>) : null} 
+					{this.state.step === 3 ? (<span>数据治理概况 - 数据资产</span>) : null} 
+				</div>
+				<div className='divider'/>
 				{this.state.step === 1 ? stepOne : null}
 				{this.state.step === 2 ? stepTwo : null}
 				{this.state.step === 3 ? stepThree : null}
-  		</div>
-  	)
+			</div>
+		)
 	}
 
 }
